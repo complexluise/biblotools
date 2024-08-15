4c044c489a162cfa131f2e005514ba7ed7eb25df
complexluise <lhiguerac@unal.edu.co>
2024-08-15 17:47:46 -0500

    add more image to message

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import base64
 import os
-
 from anthropic import Anthropic
 
 
@@ -14,19 +13,22 @@ def encode_image_to_base64(binary_data):
     return base_64_encoded_data.decode('utf-8')
 
 
-def create_message_list(base64_string, prompt):
-    return [
-        {
-            "role": 'user',
-            "content": [
-                {"type": "image", "source": {"type": "base64", "media_type": "image/jpeg", "data": base64_string}},
-                {"type": "text", "text": prompt}
-            ]
-        }
-    ]
+def create_message_list(image_data_list, prompt):
+    content = []
+    for image_data in image_data_list:
+        content.append({
+            "type": "image",
+            "source": {
+                "type": "base64",
+                "media_type": "image/jpeg",
+                "data": image_data
+            }
+        })
+    content.append({"type": "text", "text": prompt})
+    return [{"role": 'user', "content": content}]
 
 
-def generate_sonnet(client, model_name, message_list):
+def generate_response(client, model_name, message_list):
     response = client.messages.create(
         model=model_name,
         max_tokens=2048,
@@ -38,15 +40,24 @@ def generate_sonnet(client, model_name, message_list):
 def main():
     API_KEY = os.getenv("ANTHROPIC_KEY")
     client = Anthropic(api_key=API_KEY)
-    MODEL_NAME = "claude-3-opus-20240229"
-    IMAGE_PATH = "image/WhatsApp Image 2024-08-05 at 23.53.32_6c7a93e6.jpg"
-    PROMPT = "Please write a CSV file with the image, lets think in a library catalog ontology"
+    MODEL_NAME = "claude-3-5-sonnet-20240620"
+    IMAGE_PATHS = [
+        "image/WhatsApp Image 2024-08-05 at 23.53.32_6c7a93e6.jpg",
+        "image/WhatsApp Image 2024-08-05 at 23.54.04_56c2bab1.jpg",
+    ]
+    PROMPT = ("Please extract the information of the book from these images.\n"
+              "Important: include the ISBN of the book.\n"
+              "The Output is a Json")
 
-    binary_data = read_image(IMAGE_PATH)
-    base64_string = encode_image_to_base64(binary_data)
-    message_list = create_message_list(base64_string, PROMPT)
-    sonnet = generate_sonnet(client, MODEL_NAME, message_list)
-    print(sonnet)
+    image_data_list = []
+    for image_path in IMAGE_PATHS:
+        binary_data = read_image(image_path)
+        base64_string = encode_image_to_base64(binary_data)
+        image_data_list.append(base64_string)
+
+    message_list = create_message_list(image_data_list, PROMPT)
+    response = generate_response(client, MODEL_NAME, message_list)
+    print(response)
 
 
 if __name__ == "__main__":
